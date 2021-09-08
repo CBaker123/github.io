b1c96ecda621acc456940454e75bf8f6ebed86e5
CBaker123 <88594591+CBaker123@users.noreply.github.com>
2021-09-08 13:03:40 -0400

    Clean forms

---
+++ Security-Co-Test-Site/javascript.js
@@ -0,0 +1,19 @@
+
+// Request Quote Data
+// id="firstName"
+// id="lastName"
+// id="email"
+// id="phoneNumber"
+// id="requestServiceType"
+// id="myQuoteButton"
+// 
+// Employment Application Data
+// id="firstName2"
+// id="lastName2"
+// id="email2"
+// id="phoneNumber2"
+// id="employmentEducation"
+// id="employmentExperience"
+// id="coverLetterFile"
+// id="resumeFile"
+// id="myApplicationButton"
