740c71034919bc693b305131ed8b82747bd60cbd
CBaker123 <88594591+CBaker123@users.noreply.github.com>
2021-11-06 12:29:43 -0400

    map commit 2

--- Shipping-Test-Site/map.js
+++ Shipping-Test-Site/map.js
@@ -1,3 +1,5 @@
+
+function createMap(){
 var map = new ol.Map({
   target: 'map',
   layers: [
@@ -6,7 +8,20 @@ var map = new ol.Map({
     })
   ],
   view: new ol.View({
-    center: ol.proj.fromLonLat([37.41, 8.82]),
+    center: ol.proj.fromLonLat([ -80.89511, 40.03637]),
     zoom: 4
   })
 });
+}
+function searchZip (){
+  var zip = document.getElementById("zipItem").value;
+  var latLong = getLatLong(zip);
+  console.log(zip);
+}
+
+// function getLatLong(zip) {
+//   var apiAddress =  'http://api.positionstack.com/v1/forward';
+//     var access_key = '4c53e0724e37dffb9853c9a9d4fcdb9b'
+//     var query = zip;
+//     return place;
+// }
