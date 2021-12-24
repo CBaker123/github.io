738fea7362c8b37de2d566f2189c3627eb17f018
CBaker123 <88594591+CBaker123@users.noreply.github.com>
2021-12-24 14:01:24 -0500

    guess game

---
+++ Guessing Game/javascript.js
@@ -0,0 +1,54 @@
+let numberOfGuessesLeft = 20;
+let secretNumber = Math.floor(Math.random() * 20 + 1);
+let highScore = 0;
+console.log(secretNumber);
+
+function youWin() {
+  document.getElementById("message").innerHTML = "you win";
+  document.getElementById("secretNumber").innerHTML = secretNumber;
+}
+
+function youLose() {
+  document.getElementById("guessesLeft").innerHTML = "<p> 0 </p>";
+  document.getElementById("message").innerHTML = "you lose";
+  document.getElementById("secretNumber").innerHTML = secretNumber;
+}
+
+function checkHighScore() {
+  if (numberOfGuessesLeft > highScore) {
+    highScore = numberOfGuessesLeft;
+    document.getElementById("highScore").innerHTML = highScore;
+  }
+}
+
+function function1() {
+  numberOfGuessesLeft -= 1;
+  let myGuess = document.getElementById("numberGuess1").value;
+
+  if (myGuess < 21 && myGuess > 0) {
+    if (numberOfGuessesLeft > 0) {
+      document.getElementById("guessesLeft").innerHTML = "<p>" + numberOfGuessesLeft + "</p>";
+      if (myGuess < secretNumber) {
+        document.getElementById("message").innerHTML = "too low";
+      } else if (myGuess > secretNumber) {
+        document.getElementById("message").innerHTML = "too high";
+      } else {
+        youWin();
+        checkHighScore();
+      }
+    } else {
+      youLose();
+    }
+  } else {
+    document.getElementById("message").innerHTML = "Not A Valid Guess";
+  }
+}
+
+function setGame(){
+  numberOfGuessesLeft = 20;
+  secretNumber = Math.floor(Math.random() * 20 + 1);
+  document.getElementById("numberGuess1").value = "";
+  document.getElementById("message").innerHTML = "Good Luck";
+  document.getElementById("secretNumber").innerHTML = "?";
+  console.log(secretNumber);
+}
